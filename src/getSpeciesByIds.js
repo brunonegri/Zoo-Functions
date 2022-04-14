e6073f540e88ecaf83e9581f8c0771ed6afd7bf8
Bruno Negri <b.negri_@hotmail.com>
2022-04-14 17:28:49 -0300

    Requesito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui ja vou começar
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  }
+  const filtradas = species.filter((specie) => ids.find((id) => id === specie.id));
+  return filtradas;
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
