ddc9781bbc0c9949d15403d7a1aeaf3f71e5de99
Bruno Negri <b.negri_@hotmail.com>
2022-04-14 23:46:25 -0300

    Requesito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const nome = employees[7].firstName;
+  if (nome === employeeName) {
+    const firstName = employees.find((empregado) => empregado.firstName === employeeName);
+    return firstName;
+  }
+  const lastName = employees.find((empregado) => empregado.lastName === employeeName);
+  return lastName;
 }
+
+console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
