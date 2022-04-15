f393967c561003e436aa4ce12d4de9d6cb7e5ec5
Bruno Negri <b.negri_@hotmail.com>
2022-04-15 09:49:07 -0300

    Requesito 3 refeito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -6,12 +6,12 @@ function getEmployeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
-  const nome = employees[7].firstName;
-  if (nome === employeeName) {
-    const firstName = employees.find((empregado) => empregado.firstName === employeeName);
+  const firstName = employees.find((empregado) => empregado.firstName === employeeName);
+  const lastName = employees.find((empregado) => empregado.lastName === employeeName);
+  const verify = employees.some((empregado) => empregado.firstName.includes(employeeName));
+  if (verify === true) {
     return firstName;
   }
-  const lastName = employees.find((empregado) => empregado.lastName === employeeName);
   return lastName;
 }
 
