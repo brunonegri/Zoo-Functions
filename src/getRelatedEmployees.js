07c7a8ac434051909413234fdcabef675eb5f662
Bruno Negri <b.negri_@hotmail.com>
2022-04-16 11:45:31 -0300

    Requesito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -8,14 +8,25 @@ function isManager(id) {
   return verify;
 }
 
-console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
+// console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 // EMPREGADO '4b40a139-d4dc-4f09-822d-ec25e819a5ad'
 // GERENTES '9e7d4524-363c-416a-8759-8aa7e50c0992' 'fdb2543b-5662-46a7-badc-93d960fdc0a8' '0e7b460e-acf4-4e17-bcb3-ee472265db83'
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
-
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  try {
+    const responsavel = employees.filter((employee) => employee.managers.includes(managerId));
+    const retornaNomes = responsavel.map((atual) => `${atual.firstName} ${atual.lastName}`);
+    return retornaNomes;
+  } catch (error) {
+    throw error.message;
+  }
 }
+
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
