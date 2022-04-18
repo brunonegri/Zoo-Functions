90618bf6c3d2708d53d482ca7d5d147e10c6f755
Bruno Negri <b.negri_@hotmail.com>
2022-04-18 15:32:57 -0300

    start day 2

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,16 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || {}) {
+    return 0;
+  }
 }
 
 module.exports = { calculateEntry, countEntrants };
