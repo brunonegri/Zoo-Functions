c243e536e4e5a5f9de8f03f891c5f1c828e3ecf0
Bruno Negri <b.negri_@hotmail.com>
2022-04-19 16:38:59 -0300

    Requesito 9 feito req 8 90%

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const colaborador = employees.find((employee) => employee.id === id);
+  const animais = species.find((specie) => specie.id.includes(colaborador.responsibleFor[0]))
+    .residents.sort((a, b) => b.age - a.age)[0];
+  return [animais.name, animais.sex, animais.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,9 +2,30 @@ const data = require('../data/zoo_data');
 
 const { species, hours } = data;
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+// const objeto = {};
+const filtraDia = Object.keys(hours);
+// console.log(filtraDia);
 
+function getSchedule(param) {
+  if (!param || !species.some((specie) => specie.availability.includes(param))) {
+    const objeto = species.forEach((atual) => ({ [filtraDia[0]]: { officeHour: hours.Tuesday.open, exhibition: atual.availability } }));
+    return objeto;
+  }
+  if (species.some((specie) => specie.name.includes(param))) {
+    return species.filter((specie) => specie.name.includes(param))[0].availability;
+  }
+  if (param === 'Monday') {
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  }
+  const filtro1 = species.filter((specie) => specie.availability.includes(param));
+  const filtro2 = filtro1.reduce((acc, specie) => {
+    acc.exhibition.push(specie.name);
+    acc.officeHour = `Open from ${hours[param].open}am until ${hours[param].close}pm`;
+    return acc;
+  }, { officeHour: '', exhibition: [] });
+  return { [param]: filtro2 };
 }
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
