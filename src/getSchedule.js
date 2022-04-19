9e0aea4590a2bc3dd0cfbaac35dfe71540fd565e
Bruno Negri <b.negri_@hotmail.com>
2022-04-19 20:31:14 -0300

    Requesito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,30 +2,48 @@ const data = require('../data/zoo_data');
 
 const { species, hours } = data;
 
-// const objeto = {};
-const filtraDia = Object.keys(hours);
-// console.log(filtraDia);
+const semana = Object.keys(hours);
+const filtraDia = (atual) => species.filter((dia) => dia
+  .availability.includes(atual)).map((atual2) => atual2.name);
+
+const objeto = {};
+
+const cronogramaSemParametro = () => {
+  semana.forEach((atual) => {
+    if (atual !== 'Monday') {
+      objeto[atual] = {
+        officeHour: `Open from ${data.hours[atual].open}am until ${data.hours[atual].close}pm`,
+        exhibition: filtraDia(atual),
+      };
+    } else {
+      objeto[atual] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    }
+  });
+  return objeto;
+};
 
 function getSchedule(param) {
-  if (!param || !species.some((specie) => specie.availability.includes(param))) {
-    const objeto = species.forEach((atual) => ({ [filtraDia[0]]: { officeHour: hours.Tuesday.open, exhibition: atual.availability } }));
-    return objeto;
-  }
   if (species.some((specie) => specie.name.includes(param))) {
     return species.filter((specie) => specie.name.includes(param))[0].availability;
+  }
+  if (semana.includes(param) && param !== 'Monday') {
+    const filtro1 = species.filter((specie) => specie.availability.includes(param));
+    const filtro2 = filtro1.reduce((acc, specie) => {
+      acc.exhibition.push(specie.name);
+      acc.officeHour = `Open from ${hours[param].open}am until ${hours[param].close}pm`;
+      return acc;
+    }, { officeHour: '', exhibition: [] });
+    return { [param]: filtro2 };
   }
   if (param === 'Monday') {
     return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
   }
-  const filtro1 = species.filter((specie) => specie.availability.includes(param));
-  const filtro2 = filtro1.reduce((acc, specie) => {
-    acc.exhibition.push(specie.name);
-    acc.officeHour = `Open from ${hours[param].open}am until ${hours[param].close}pm`;
-    return acc;
-  }, { officeHour: '', exhibition: [] });
-  return { [param]: filtro2 };
+  return cronogramaSemParametro();
 }
 
-console.log(getSchedule());
+console.log(getSchedule('Monday'));
 
 module.exports = getSchedule;
